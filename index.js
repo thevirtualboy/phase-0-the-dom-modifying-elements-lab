144b1fa5f6eae00eef3a8d3d553b692600df0bbd
Peter Fontana <dude0508@gmail.com>
2021-09-12 16:57:56 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+const main = document.getElementById('main');
+main.remove();
+
+const newHeader = document.createElement('h1');
+
+newHeader.setAttribute('id', 'victory');
+
+newHeader.innerHTML = "<h1>Peter is the champion!</h1>";
+
+document.body.append(newHeader);
